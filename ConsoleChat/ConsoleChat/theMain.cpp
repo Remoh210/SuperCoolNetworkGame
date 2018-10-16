8197fbebf018003a03ff1e6e5c0642fa8e1f6192
Bogdan <bondarenkobogdankiev@gmail.com>
2018-10-16 17:32:23 -0400

    Basic UI

--- ConsoleChat/ConsoleChat/theMain.cpp
+++ ConsoleChat/ConsoleChat/theMain.cpp
@@ -6,6 +6,7 @@
 #include <ws2tcpip.h>
 #include <iostream>
 #include <vector>
+#include "ConnectionMaintainer.h"
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Mswsock.lib")
 #pragma comment(lib, "AdvApi32.lib")
@@ -14,7 +15,44 @@
 #define SERVER_ADDRESS "127.0.0.1"
 #define ARRAY_SIZE 128  
 
+char Answer;
+
 int main() {
+	UserInfo User;
+
+	std::cout << "Connect to the loacal server? \n";
+	std::cout << "Type \"y\" or \"n\": ";
+	std::cin >> Answer;
+
+
+	if (Answer == 'y' || Answer == 'Y') {
+		std::cout << "Connecting to the localhost \n";
+		User.ServerAdress = SERVER_ADDRESS;
+	}
+	else if (Answer == 'n' || Answer == 'N') {
+		char serverAdress[ARRAY_SIZE];
+		std::cout << "Specify the adress: \n";
+		std::cin >> serverAdress;
+		User.ServerAdress = serverAdress;
+	}
+	else {
+		std::cout << "Incorrect answer\n";
+	};
+
+	std::cout << "Enter Your Name: \n";
+	char FirstName[ARRAY_SIZE];
+	std::cin >> FirstName;
+	User.Name = FirstName;
+
+	// Buffer send
+	Buffer* Send_Buffer = new Buffer(BUFFER_LENGTH);
+
+	// Buffer recieve
+	Buffer* Recieve_Buffer = new Buffer(BUFFER_LENGTH);
+
+	ConnectionMaintainer Conn;
+	Conn.Connect(User);
+
 
 	return 0;
 }
