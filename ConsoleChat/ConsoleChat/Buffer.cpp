5bbea3f1b921b85241a55c37cb179d90159d7dd2
Bogdan <bondarenkobogdankiev@gmail.com>
2018-10-16 16:45:18 -0400

    Int and Short serialization added

--- ConsoleChat/ConsoleChat/Buffer.cpp
+++ ConsoleChat/ConsoleChat/Buffer.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 
+#define MAX_GROW 100
 
 Buffer::Buffer(unsigned int size) : mWriteIndex(0), mReadIndex(0) {
 	mBuffer.resize(size);
@@ -31,4 +32,63 @@ void Buffer::clearBuffer() {
 	this->mBuffer = "";
 	this->mReadIndex = 0;
 	this->mWriteIndex = 0;
+}
+
+
+void Buffer::WriteInt32LE(unsigned int index, int value) {
+	// Is it large enough?
+	if (index <= this->mBuffer.size() - 4) this->mBuffer.resize(index + MAX_GROW);
+
+	char firstByteBE = (value & 0x000000FF) >> 0;
+	char secondByteBE = (value & 0x0000FF00) >> 8;
+	char thirdByteBE = (value & 0x00FF0000) >> 16;
+	char fourthByteBE = (value & 0xFF000000) >> 24;
+
+	this->mBuffer[index + 0] = firstByteBE;
+	this->mBuffer[index + 1] = secondByteBE;
+	this->mBuffer[index + 2] = thirdByteBE;
+	this->mBuffer[index + 3] = fourthByteBE;
+}
+
+void Buffer::WriteInt32LE(int value) {
+	// Is large enough?
+	if (this->mWriteIndex <= this->mBuffer.size() - 4)
+		this->mBuffer.resize(this->mWriteIndex + MAX_GROW);
+
+	char firstByteBE = (value & 0x000000FF) >> 0;
+	char secondByteBE = (value & 0x0000FF00) >> 8;
+	char thirdByteBE = (value & 0x00FF0000) >> 16;
+	char fourthByteBE = (value & 0xFF000000) >> 24;
+
+	this->mBuffer[this->mWriteIndex + 0] = firstByteBE;
+	this->mBuffer[this->mWriteIndex + 1] = secondByteBE;
+	this->mBuffer[this->mWriteIndex + 2] = thirdByteBE;
+	this->mBuffer[this->mWriteIndex + 3] = fourthByteBE;
+
+	this->mWriteIndex += 4;
+}
+
+void Buffer::WriteInt16LE(unsigned int index, short value) {
+	// Is large enough?
+	if (index <= this->mBuffer.size() - 2) this->mBuffer.resize(index + MAX_GROW);
+
+	char firstByteBE = (value & 0x00FF) >> 0;
+	char secondByteBE = (value & 0xFF00) >> 8;
+
+	this->mBuffer[index + 0] = firstByteBE;
+	this->mBuffer[index + 1] = secondByteBE;
+}
+
+void Buffer::WriteInt16LE(short value) {
+	// Is large enough?
+	if (this->mWriteIndex <= this->mBuffer.size() - 2)
+		this->mBuffer.resize(this->mWriteIndex + MAX_GROW);
+
+	char firstByteBE = (value & 0x00FF) >> 0;
+	char secondByteBE = (value & 0xFF00) >> 8;
+
+	this->mBuffer[mWriteIndex + 0] = firstByteBE;
+	this->mBuffer[mWriteIndex + 1] = secondByteBE;
+
+	this->mWriteIndex += 2;
 }
