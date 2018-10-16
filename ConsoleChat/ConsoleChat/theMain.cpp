cbafc760ba5dca757c735829a4f162a072a1f7f2
Bogdan <bondarenkobogdankiev@gmail.com>
2018-10-16 17:41:59 -0400

    finished client side

--- ConsoleChat/ConsoleChat/theMain.cpp
+++ ConsoleChat/ConsoleChat/theMain.cpp
@@ -16,6 +16,7 @@
 #define ARRAY_SIZE 128  
 
 char Answer;
+void SwitchRoom(UserInfo& cData);
 
 int main() {
 	UserInfo User;
@@ -54,5 +55,69 @@ int main() {
 	Conn.Connect(User);
 
 
+	if (Conn.isAlive) {
+		cout << "connected!\n"
+			<< "-j -l Join and Leave";
+		system("pause");
+	}
+	else {
+		cout << "Server is down.\n";
+		Sleep(6000);
+	}
+
+
+	string Send_Message;
+	string Recieve_Message;
+	string ChatBuffer;
+	vector<string> ConnRooms;
+
+
+	while (Conn.isAlive) {
+		Recieve_Message = Conn.getMessages();
+
+		// Checking messege from the server
+		if (Recieve_Message != "") {
+			ChatBuffer += Recieve_Message;
+
+			cout << ChatBuffer;
+			cout << "\n";
+			cout << "-j means join, -l means leave\n";
+			cout << "Use the Enter key to Update\n";
+			cout << "Connected to ";
+			for (int i = 0; i < ConnRooms.size(); i++) cout << ConnRooms[i] << " ";
+			cout << '\n';
+			cout << "\n";
+		}
+
+		getline(cin, Send_Message);
+
+		if (Send_Message == "-j") {
+			Send_Message = "";
+			SwitchRoom(User);
+			ConnRooms.push_back(User.Room);
+			Conn.sendMessage(Send_Buffer, User, MSG_ID_JOIN_ROOM, Send_Message);
+		}
+		if (Send_Message == "-l") {
+			Send_Message = "";
+			SwitchRoom(User);
+			for (int i = 0; i < ConnRooms.size(); i++) {
+				if (ConnRooms.at(i) == User.Room) ConnRooms.at(i).erase();
+			}
+			Conn.sendMessage(Send_Buffer, User, MSG_ID_LEAVE_ROOM, Send_Message);
+		}
+		if (Send_Message != "") {
+			Conn.sendMessage(Send_Buffer, User, MSG_ID_LEAVE_THE_MESSAGE, Send_Message);
+		}
+	}
+
+
 	return 0;
+}
+
+void SwitchRoom(UserInfo& cData) {
+	cout << "Switch the room by typing its number: \n";
+	char* answer;
+	answer = new char[ARRAY_SIZE];
+	cin >> answer;
+	cData.Room = answer;
 }
